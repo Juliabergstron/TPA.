976e04518ff906d4cc11cce2e5ae193e20d0d6a6
Juliabergstron <bergstronjuliacosta@gmail.com>
2025-04-09 13:43:16 -0300

    Add files via upload

---
+++ TPAdadosPrimitivos/src/main/java/TPAdadosPrimitivos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class TPAdadosPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    /**primeiro programa de java*/
+/**Eu Juia Bergstron estou criando meu primeiro algoritimo*/
+//Declarndo dados primitivos no NetBeans
+
+            byte idadeNoAno = 25; //Armazena idades (valores pequenos)
+            short ano = 2025; //Armazena anos (valores razoavélmente maiores) 
+            int QuantidadePopulacionalAmostra = 500000; // tipos de números grandes, mas comuns
+            long QuantidadePopulacionalMundial = 8000000000L; // tipos de números muito grandes e específicos
+            
+            float SuaAltura = 1.75f; // vai receber float 
+            double SeuPeso = 72.5;  // Mais preciso que o float usado para diferenciar
+            
+            char PrimeiraLetra = 'J';// Guandar apenas UM algarismo
+            boolean Funcionando = true; // Mostra se é verdadeiro ou falso
+           
+            //Agora iremos fazer o algoritimo para exibri os valores da variável
+            System.out.println("Idade: " + idadeNoAno);
+            System.out.println("Ano: " + ano);
+            System.out.println("População Amostra: " + QuantidadePopulacionalAmostra);
+            System.out.println("População Mundial: " + QuantidadePopulacionalMundial);
+            System.out.println("Sua Altura: " + SuaAltura + "m");
+            System.out.println("Seu Peso: " + SeuPeso + "kg" );
+            System.out.println("Primeira Letra do Nome: " + PrimeiraLetra);
+            System.out.println("Está ativo? " + Funcionando);
+    }
+    
+}
