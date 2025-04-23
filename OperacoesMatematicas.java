1d865337ec045c7f11548ee6c12a394d6e8d0b3c
Juliabergstron <bergstronjuliacosta@gmail.com>
2025-04-23 14:52:41 -0300

    Add files via upload

---
+++ OperacoesMatematicas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operacoesmatematicas;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class OperacoesMatematicas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
