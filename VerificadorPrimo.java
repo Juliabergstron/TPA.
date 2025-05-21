cacdd42df8216d9646845d4753abb84b8a0765a3
Juliabergstron <bergstronjuliacosta@gmail.com>
2025-05-21 13:50:16 -0300

    Add files via upload

---
+++ VerificadorPrimo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.verificadorprimo;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+import java.util.Scanner;
+public class VerificadorPrimo {
+
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        String resposta;
+
+        do {
+            System.out.print("Digite um número inteiro: ");
+            int numero = input.nextInt();
+
+            boolean ehPrimo = true;
+
+            if (numero <= 1) {
+                ehPrimo = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(numero); i++) {
+                    if (numero % i == 0) {
+                        ehPrimo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (ehPrimo) {
+                System.out.println(numero + " é primo.");
+            } else {
+                System.out.println(numero + " não é primo.");
+            }
+
+            System.out.print("Deseja testar outro número? (s/n): ");
+            resposta = input.next().toLowerCase();
+
+        } while (resposta.equals("s"));
+
+        input.close();
+    }
+}
