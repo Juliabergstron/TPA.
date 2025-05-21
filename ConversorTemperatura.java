f54801e6268c994869b50d77f5c5a46d78a0f23e
Juliabergstron <bergstronjuliacosta@gmail.com>
2025-05-21 13:31:02 -0300

    Add files via upload

---
+++ ConversorTemperatura.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversortemperatura;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+import java.util.Scanner;
+public class ConversorTemperatura {
+
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+
+        System.out.println("=== CONVERSOR DE TEMPERATURA ===");
+
+        System.out.print("Digite a temperatura em Celsius: ");
+        double celsius = input.nextDouble();
+
+        double fahrenheit = (celsius * 9/5) + 32;
+        double kelvin = celsius + 273.15;
+
+        System.out.println("Temperatura em Fahrenheit: " + fahrenheit + " °F");
+        System.out.println("Temperatura em Kelvin: " + kelvin + " K");
+
+        input.close();
+    }
+}
