3e2197e8b91fa956d50a375fff7d39ffdd24407a
Julia Bergstron <julia.bergstron@etec.sp.gov.br>
2026-02-19 12:09:05 -0300

    Atualizar o CalculoDeRaiz.java

--- CalculoDeRaiz.java
+++ CalculoDeRaiz.java
@@ -11,11 +11,11 @@ public class CalculoDeRaiz {
     double b = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o valor de B:"));
     double c = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o valor de C:"));
     
-    double potencia = Math.sqrt( Math.pow(b,2));
-    double mult = (4*a*c);
-    double delta =(potencia - mult);
+    double delta = Math.pow(b, 2) - 4 * a * c;
+
+
+    Math.sqrt(delta); 
     
-    
-    JOptionPane.showMessageDialog(null, "O valor da raiz é " + potencia  );
+    JOptionPane.showMessageDialog(null, "O valor da raiz é " + delta  );
     }
 }
