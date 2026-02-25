3d9fe65f20d4921e6b9f6388f8a7fd19438d2afb
Julia Bergstron <julia.bergstron@etec.sp.gov.br>
2026-02-24 22:27:09 -0300

    Implement delta checks and root calculations
    Added handling for negative delta and calculated roots.

--- CalculoDeRaiz.java
+++ CalculoDeRaiz.java
@@ -11,11 +11,17 @@ public class CalculoDeRaiz {
     double b = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o valor de B:"));
     double c = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite o valor de C:"));
     
-    double delta = Math.pow(b, 2) - 4 * a * c;
+    double delta = Math.pow(b, 2) - (4 * a * c);
 
-
-    Math.sqrt(delta); 
-    
-    JOptionPane.showMessageDialog(null, "O valor da raiz é " + delta  );
+    if(delta < 0) {
+    JOptionPane.showMessageDialog(null, "O valor do delta é negativo, não existe raiz. " );
+    } else if (delta == 0) {
+    double x = (-b)/(2*a);
+    JOptionPane.showMessageDialog(null, "O valor da raiz é " + x  );
+    }else if (delta >0){
+    double x1 = (-b + Math.sqrt(delta))/ (2*a); 
+    double x2 = (-b - Math.sqrt(delta))/(2*a); 
+    JOptionPane.showMessageDialog(null, "O valor do x1 é " + x1 +  " e o valor do x2 é " + x2  );
+    }
     }
 }
